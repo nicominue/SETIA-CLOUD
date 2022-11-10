71188b43dee6fa2d76793e2f664eb7182fd6e32d
rob3rts299 <roberto_pereyr@hotmail.com>
2022-11-10 00:40:09 -0300

    Prototype completed
    falta poner el event del boton de la orden.

--- classes/lightbox.js
+++ classes/lightbox.js
@@ -28,21 +28,37 @@ const boton = document.querySelector('.btn-turno');
     hamburguer1.style.opacity = '0';
     const valorsrc = imageneslight.src;
     switch (valorsrc){
-      case 'http://127.0.0.1:5500/build/img/family.jpg':
-         titulo.textContent = "pediatra";
-         descripcion.textContent = "tratamos con niños, los ayudamos bla bla bla";
+      case 'http://127.0.0.1:5500/build/img/pediatria.jpg':
+         titulo.textContent = "pediatria";
+         descripcion.textContent = "La pediatría es la especialidad médica encargada de la salud física, mental y emocional de los/as niños/as desde el nacimiento hasta la adolescencia.";
       break;
-      case 'http://127.0.0.1:5500/build/img/family2.jpg':
-         titulo.textContent = "psicologo";
-         descripcion.textContent = "tratamos todos los problemas, los ayudamos bla bla bla";
+      case 'http://127.0.0.1:5500/build/img/neumonologia.jpg':
+         titulo.textContent = "Neumonologia";
+         descripcion.textContent = "La neumonologia se ocupa del tratamiento y las enfermedades de los pulmones y las vías respiratorias.";
          break;
-      case 'http://127.0.0.1:5500/build/img/family3.jpg':
-         titulo.textContent = "terapeuta";
-         descripcion.textContent = "tratamos todos los sintomas, los ayudamos bla bla bla";
+      case 'http://127.0.0.1:5500/build/img/cardiologia.jpg':
+         titulo.textContent = "Cardiologia";
+         descripcion.textContent = "La cardiologia se encarga del estudio, prevención, diagnóstico y tratamiento de las enfermedades del corazón y del sistema circulatorio.";
          break;
-      case 'http://127.0.0.1:5500/build/img/destacada.jpg':
-         titulo.textContent = "traumatologo";
-         descripcion.textContent = "tratamos todos los huesos, los ayudamos bla bla bla";
+      case 'http://127.0.0.1:5500/build/img/traumatologia.jpg':
+         titulo.textContent = "Traumatologia";
+         descripcion.textContent = "La traumatologia se dedica al estudio de las lesiones del aparato locomotor (como es el caso de huesos y músculos). La especialidad es tratar, diagnosticar y prevenir aquellas enfermedades directamente relacionadas con los huesos, músculos o articulaciones.";
+         break;
+      case 'http://127.0.0.1:5500/build/img/ginecologia.jpg':
+         titulo.textContent = "Ginecologia";
+         descripcion.textContent = "La ginecologia se especializa en la atención de las mujeres durante el embarazo y el parto, y en el diagnóstico y tratamiento de enfermedades de los órganos reproductivos femeninos.";
+         break;
+      case 'http://127.0.0.1:5500/build/img/dermatologia.jpg':
+         titulo.textContent = "Dermatologia";
+         descripcion.textContent = "La dermatologia se especializa en el diagnóstico, tratamiento y manejo de las enfermedades y desórdenes de la piel.";
+         break;
+      case 'http://127.0.0.1:5500/build/img/urologia.jpg':
+         titulo.textContent = "Urologia";
+         descripcion.textContent = "La urología es la especialidad médico-quirúrgica que se ocupa del estudio, diagnóstico y tratamiento de las patologías que afectan al aparato urinario, glándulas suprarrenales y retroperitoneo de ambos sexos, y el aparato reproductor masculino.";
+         break;
+      case 'http://127.0.0.1:5500/build/img/nutricion.jpg':
+         titulo.textContent = "Nutricion";
+         descripcion.textContent = "Un nutricionista, dietista-nutricionista, nutriólogo, dietista o dietólogo es un profesional sanitario experto en alimentación, nutrición y dietética.";
          break;
     }
  }
